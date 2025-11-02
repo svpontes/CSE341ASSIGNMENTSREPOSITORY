73b6b07cbccb4b06653211da7292d628d94198bc
svpontes <svpontes2015@gmail.com>
2025-11-02 00:32:28 +0000

    try catch block added for getDataById

--- contactsProject/backend/controllers/friends.js
+++ contactsProject/backend/controllers/friends.js
@@ -1,17 +1,5 @@
 const mongodb = require('../db/connect');//imports the db access module
-
-const getData = async (req, res, next) => {//get data async way
-  const result = await mongodb.getDb().collection('friends').find(); //result waits from the connect to the db and get the collection friends executing the query 'find'
-  //format data and send a reposnse HTTP
-  result.toArray().then((lists) => { //convert result int an array
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]); //send HTTP response: STATUS 200 (OK) and the first element of the collection (JSON format)
-  });
-};
-
-
-
-//pergunta no ai: aqui retorna um item de collection 'friends'. ajude me a buscar totdos os itens, mas uma const separada desta:
+const { ObjectId } = require('mongodb');//import the objectId from mongodb 
 
 const getAllData = async (req, res, next) => {//get data async way
   const result = await mongodb.getDb().collection('friends').find(); //result waits from the connect to the db and get the collection friends executing the query 'find'
@@ -22,4 +10,52 @@ const getAllData = async (req, res, next) => {//get data async way
   });
 };
 
-module.exports = { getData, getAllData };//exposts the functions to be used by Router
+//get itens by id
+/*const getSingleById = async (req, res, next) => {
+  
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().collection('friends').find({ _id: userId });
+
+  result.toArray().then((lists) => {
+    if (lists.length > 0) {
+      res.setHeader('Content-type', 'application/jason');
+      res.status(200).json(lists[0]);
+    } else {
+      res.status(404).json({ message: 'Friend not found with ID: ' + req.params.id });
+    }
+  }).catch(err => {
+    res.status(500).json({ message: 'Error retrieving friend!', error: err.message });
+  });
+};*/
+
+//nova tentaiva com try catch block:
+
+const getSingleById = async (req, res, next) => {
+    // 1. [ADICIONADO] Bloco try...catch para tratar o BSONError
+    try {
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().collection('friends').find({ _id: userId });
+
+        result.toArray().then((lists) => {
+            if (lists.length > 0) {
+                res.setHeader('Content-type', 'application/json');
+                res.status(200).json(lists[0]);
+            } else {
+                // ID válido, mas não encontrado no DB
+                res.status(404).json({ message: 'Friend not found with ID: ' + req.params.id });
+            }
+        });
+    } catch (err) {
+        
+        if (err.name === 'BSONError' || err.message.includes('must be a 24 character hex string')) {
+            return res.status(400).json({ 
+                message: 'Invalid ID format provided.',
+                details: 'The ID must be a valid 24-character hexadecimal string.'
+            });
+        }
+        // Trata outros erros internos
+        res.status(500).json({ message: 'Error retrieving friend!', error: err.message });
+    }
+};
+
+module.exports = {getAllData, getSingleById };//exposts the functions to be used by Router
--- contactsProject/backend/routes/app.js
+++ contactsProject/backend/routes/app.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();//istantiate Router from express ()
+const router = require('express').Router();//instantiate Router from express ()
 const friendsController = require('../controllers/friends');//import all logic from friends.js (module getData)
-
-//first endpoint /friends/ (return one item from the list friends)
-router.get('/', friendsController.getData);//map the Router root path to friends in the server to the module getData(return one item from the list)
 
 //second endpoint /friends/all (return all itens from the list friends)
 router.get('/all', friendsController.getAllData);//map the Router root path to friends in the server to the module getAllData(return all itens from the list)
 
+//first endpoint /friends/:id (return all itens from the list friends)
+router.get('/:id', friendsController.getSingleById);//map the Router root path to friends in the server to the module getAllData(return all itens from the list)
+
+
+router.get('/', friendsController.getAllData); // Rota raiz do Router
 module.exports = router; //exports the Router to be used in server.js
